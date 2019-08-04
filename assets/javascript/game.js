d23d4a6cb6b35772a9b847dd611147a6a8464c3c
Jason Ostergren <jason@Jasons-MacBook-Air.local>
2019-08-04 08:43:30 -0500

    adjusting HTML Layout, added css, pseudo coded javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+// define variables
+
+let wins = 0;
+let losses = 0;
+
+
+// function for random number generator that resets after win or lose.
+
+// function for random crystal generator that resets after win or lose.
+
+// function for determining player number using combined crystal click
+    
+// function for if the player number matchs win condition else lose
+
+// section for updating HTML text content with updating variables
