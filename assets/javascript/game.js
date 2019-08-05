da72ad2bc1361fa5f60a58b13cfccec0cd6249e7
Jason Ostergren <jason@Jasons-MacBook-Air.local>
2019-08-04 19:11:37 -0500

    updating with additional javascript, added pseudo code, updated styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,37 @@
-// define variables
 
-let wins = 0;
-let losses = 0;
+// variables
 
 
-// function for random number generator that resets after win or lose.
+const randnumber = [10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
+const cryst1 = [1,2,3,4,5,6,7,8,9,10]
+const cryst2 = [1,2,3,4,5,6,7,8,9,10]
+const cryst3 = [1,2,3,4,5,6,7,8,9,10]
+const cryst4 = [1,2,3,4,5,6,7,8,9,10]
 
-// function for random crystal generator that resets after win or lose.
 
-// function for determining player number using combined crystal click
-    
-// function for if the player number matchs win condition else lose
+// need to set up a start and clear scenario for when the game is won or lost
 
-// section for updating HTML text content with updating variables
+// started random number generator with for loop 
+
+for (let i = 0; i < randnumber.length; i++) {
+
+    const randnumbersbtn = document.createElement("rndbtn");
+
+}
+
+// started on-click event for crystals
+
+document.querySelectorAll(".crystalImages").forEach(function(node){
+    node.addEventListener("click", function() {
+
+        alert("You clicked a crystal!");
+
+    });
+})
+
+// need to set up random number generated for each crystal that resets after the game is over
+
+// need if else statments to compare the randomnumber with the summation of chosen crystals to determine win or lose
+
+
+
