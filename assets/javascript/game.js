f903446aa4ccca735c00cf60c725d25015a1e39a
Jason Ostergren <jason@Jasons-MacBook-Air.local>
2019-08-05 20:56:10 -0500

    defined additional variable, configured random number box, updated css for scorebox, started work on crystal numbers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,36 +2,27 @@
 // variables
 
 
-const randnumber = [10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
-const cryst1 = [1,2,3,4,5,6,7,8,9,10]
-const cryst2 = [1,2,3,4,5,6,7,8,9,10]
-const cryst3 = [1,2,3,4,5,6,7,8,9,10]
-const cryst4 = [1,2,3,4,5,6,7,8,9,10]
+const RandomNumbers = [10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
+const crystalNumbers = [1,2,3,4,5,6,7,8,9,10]
 
 
-// need to set up a start and clear scenario for when the game is won or lost
+let randomnumberdiv = document.getElementById("RandomNumber")
+let winsdiv = document.getElementById("wins")
+let lossesdiv = document.getElementById("losses")
 
-// started random number generator with for loop 
+let crystal1div = document.getElementById("crystalimage1")
+let crystal2div = document.getElementById("crystalimage2")
+let crystal3div = document.getElementById("crystalimage3")
+let crystal4div = document.getElementById("crystalimage4")
 
-for (let i = 0; i < randnumber.length; i++) {
+let RandomChoice = RandomNumbers[Math.floor(Math.random()*RandomNumbers.length)]; 
+console.log(RandomChoice);
 
-    const randnumbersbtn = document.createElement("rndbtn");
+randomnumberdiv.textContent = "Random Number: " + RandomChoice;
 
-}
+let cry1 = crystalNumbers[Math.floor](Math.random()*crystalNumbers.length);
+let cry2 = crystalNumbers[Math.floor](Math.random()*crystalNumbers.length);
+let cry3 = crystalNumbers[Math.floor](Math.random()*crystalNumbers.length);
+let cry4 = crystalNumbers[Math.floor](Math.random()*crystalNumbers.length);
 
-// started on-click event for crystals
-
-document.querySelectorAll(".crystalImages").forEach(function(node){
-    node.addEventListener("click", function() {
-
-        alert("You clicked a crystal!");
-
-    });
-})
-
-// need to set up random number generated for each crystal that resets after the game is over
-
-// need if else statments to compare the randomnumber with the summation of chosen crystals to determine win or lose
-
-
-
+crystal1div.textContent = cry1
