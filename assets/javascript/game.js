16dc998318a07ac49a3391c5ff277d27270ed70a
michaelbedran <michaelbedran@users.noreply.github.com>
2019-08-05 23:49:41 -0500

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,8 +2,19 @@
 // variables
 
 
-const RandomNumbers = [10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
-const crystalNumbers = [1,2,3,4,5,6,7,8,9,10]
+var RandomNumbers = [];
+var CrystalNumbers = [];
+
+
+for (int x=10; x<=50; x++)
+{
+RandomNumbers.push(x)
+}
+
+for (int x=1; x<=10; x++)
+{
+CrystalNumbers.push(x)
+}
 
 
 let randomnumberdiv = document.getElementById("RandomNumber")
@@ -15,14 +26,14 @@ let crystal2div = document.getElementById("crystalimage2")
 let crystal3div = document.getElementById("crystalimage3")
 let crystal4div = document.getElementById("crystalimage4")
 
-let RandomChoice = RandomNumbers[Math.floor(Math.random()*RandomNumbers.length)]; 
+let RandomChoice = RandomNumbers[Math.floor(Math.random()*RandomNumbers.length -1)]; 
 console.log(RandomChoice);
 
-randomnumberdiv.textContent = "Random Number: " + RandomChoice;
+randomnumberdiv.textContent = "Random Number: " + RandomNumbers[RandomChoice];
 
 let cry1 = crystalNumbers[Math.floor](Math.random()*crystalNumbers.length);
 let cry2 = crystalNumbers[Math.floor](Math.random()*crystalNumbers.length);
 let cry3 = crystalNumbers[Math.floor](Math.random()*crystalNumbers.length);
 let cry4 = crystalNumbers[Math.floor](Math.random()*crystalNumbers.length);
 
-crystal1div.textContent = cry1
+crystal1div.textContent = CrystalNumbers[cry1];
